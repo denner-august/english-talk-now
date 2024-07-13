82aecba0be3be5d18a7c2751a51a86006672518a
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-07-13 16:43:05 -0300

    melhoria na perfomance

--- english-talk/tools/microfone.ts
+++ english-talk/tools/microfone.ts
@@ -1,9 +1,11 @@
 import SpeechRecognition from "react-speech-recognition";
+import frasesIngles from "@/words.json";
 
 class Microfone {
   start() {
     SpeechRecognition.startListening({
       continuous: true,
+      language: "en-US",
     });
   }
 
@@ -14,6 +16,19 @@ class Microfone {
   reset(newfunction: () => void) {
     this.stop();
     newfunction();
+  }
+
+  frase() {
+    const regex = /[^\w\s]/g;
+    const randomIndex = Math.floor(
+      Math.random() * frasesIngles["frases:"].length
+    );
+
+    const tratativa = frasesIngles["frases:"][randomIndex]
+      .replace(regex, "")
+      .toLowerCase();
+
+    return tratativa;
   }
 }
 
